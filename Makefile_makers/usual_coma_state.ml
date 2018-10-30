d027288e85295e638eff6991ecc83d0dda0af120
Ewan Delanoy <ewan.delanoy@zoho.com>
2018-10-30 10:08:45 +0100

    modified Usual_coma_state.

--- Makefile_makers/usual_coma_state.ml
+++ Makefile_makers/usual_coma_state.ml
@@ -11,4 +11,8 @@ let main_ref=Coma_state_field.empty_one
                 Coma_big_constant.this_world
                 Coma_big_constant.backup_dir_for_this_world;;
 
-                           
+let ucc ()=
+Coma_state.Create_or_update_copied_compiler.ucc
+  main_ref 
+ (Coma_big_constant.next_world,
+  Coma_big_constant.dummy_backup_dir);;                           
